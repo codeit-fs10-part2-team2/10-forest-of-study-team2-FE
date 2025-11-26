502004a951285665fff16e7ff14ab62c745ddf8d
Eunseoimda <rainboweunseo@gmail.com>
2025-11-26 14:03:54 +0900

    test

---
+++ src/pages/Timer/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './index.css';
+
+function Timer()
+{
+	return (
+		<div id="timer-page">
+			안녕하세요
+			<div className='title-page'>
+
+			</div>
+		</div>
+			);
+}
+
+export default Timer;
