17973cd27f1cfe7bd899bb6eab592ac36f3bc64f
Hanarin <rainboweunseo@gmail.com>
2025-12-02 15:11:05 +0900

    testtesttest

--- src/pages/TimerPage.jsx
+++ src/pages/TimerPage.jsx
@@ -1,99 +1,86 @@
-import React, { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useCallback } from "react";
 
+import style from "./TimerPage.module.css";
 
+function Timer()
+{
+	const [timer, setTimer] = useState({ id: null, time: 60 * 25 });
 
-const TimerPage = () => {
-  const INITIAL_TIME = 25 * 60;
-  const [time, setTime] = useState(INITIAL_TIME);
-  const [isRunning, setIsRunning] = useState(false);
-  const timerRef = useRef(null);
+	// memory leak!
+	useEffect(() =>
+	{
+		return () => clearInterval(timer.id);
+	},
+	[]);
 
-  useEffect(() => {
-    if (isRunning) {
-      timerRef.current = setInterval(() => {
-        setTime(prev => {
-          if (prev <= 1) {
-            clearInterval(timerRef.current);
-            return 0;
-          }
-          return prev - 1;
-        });
-      }, 1000);
-    } else {
-      clearInterval(timerRef.current);
-    }
+	const start = useCallback(() =>
+	{
+		setTimer((_) => ({ time: _.time, id: setInterval(() =>
+		// repeat every 1s
+		setTimer((_) => ({ ..._, time: _.time - 1 })), 1000) }));
+	},
+	[]);
 
-    return () => clearInterval(timerRef.current);
-  }, [isRunning]);
+	const pause = useCallback(() =>
+	{
+		clearInterval(timer.id);
+		setTimer((_) => ({ time: _.time, id: null }));
+	},
+	[]);
 
-  const formatTime = (seconds) => {
-    const m = Math.floor(seconds / 60).toString().padStart(2, "0");
-    const s = (seconds % 60).toString().padStart(2, "0");
-    return `${m}:${s}`;
-  };
+	const reset = useCallback(() =>
+	{
+		clearInterval(timer.id);
+		setTimer((_) => ({ time: 60 * 25, id: null }));
+	},
+	[]);
 
-  return (
-    <div  
-      style={{
-      backgroundColor: "white",     
-      minHeight: "100vh",           
-   }}//배경 화면 하얗게 하기
+	const format = useCallback((time) =>
+	{
+		const [mm, ss] = [(time - (time % 60)) / 60, (time % 60)]; // TODO: YY, DD
 
-    >
-    
-    
-    <ul style={{ textAlign: "center", padding: "70px" }}>
-      <h2>오늘의 집중</h2>
-      <br></br>
-      <img src="../../assets/images/icons/25_timer.png" alt="25_timer" />
-      <br></br>
-      <h1 style={{ fontSize: "80px", marginBottom: "40px", color: "red", fontWeight: "bold" }}>
-        {formatTime(time)}
-      </h1>
+		return `${mm.toString().padStart(2, "0")}:${ss.toString().padStart(2, "0")}`;
+	},
+	[]);
 
-      <div style={{ display: "flex", justifyContent: "center",  gap: "20px" }}>
-      {isRunning && (
-          <img
-            src="../../assets/images/icons/btn_pause.png"
-            alt="pause"
-        
-            style={{ cursor: "pointer" }}
-            onClick={() => setIsRunning(false)}
-          />
-        )}
+	return (
+		<div className={style.page}>
+			<div className={style.head}>
+				연우의 개발공장
+				<div className={style.goto}>
+					<a className={style.link} href="">
+						오늘의 습관
+					</a>
+					<a className={style.link} href="">
+						홈
+					</a>
+				</div>
+			</div>
+			<div className={style.info}>
+				현재까지 획득한 포인트
+				<div className={style.data}>
+					310P 획득
+				</div>
+			</div>
+			<div className={style.body}>
+				오늘의 집중
+				<div className={style.clock} style={{ color: timer.time <= 10 ? "#F50E0E" : timer.time < 0 ? "#818181" : undefined }}>
+					{format(timer.time)}
+				</div>
+				<div className={style.tools}>
+					<button className={style.pause_btn} onClick={pause} style={{ display: timer.id ? undefined : "none" }}>
+						Pause!
+					</button>
+					<button className={style.start_btn} onClick={start} disabled={timer.id != null}>
+						Start!
+					</button>
+					<button className={style.reset_btn} onClick={reset} style={{ display: timer.id ? undefined : "none" }}>
+						Reset!
+					</button>
+				</div>
+			</div>
+		</div>
+	);
+}
 
-        {!isRunning && (
-          <img
-            src="../../assets/images/icons/btn_start.png"
-            alt="reset"
-        
-            style={{ cursor: "pointer" }}
-            onClick={() => setIsRunning(true)}
-          />
-        )}
-
-        {isRunning && (
-          <img
-            src="../../assets/images/icons/btn_reset.png"
-            alt="stop"
-        
-            style={{ cursor: "pointer" }}
-            onClick={() => { setIsRunning(false);
-                             setTime(INITIAL_TIME);}
-
-            }
-          />
-        )}
-
-
-      </div>
-
-
- 
-    </ul>
-  
-    </div>
-  );
-};
-
-export default TimerPage;
+export default Timer;
