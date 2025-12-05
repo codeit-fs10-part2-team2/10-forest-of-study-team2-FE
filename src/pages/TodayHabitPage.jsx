050a9e47de2cce767830f0382407739d7b49f724
taetaehoo <hapkldo09@naver.com>
2025-12-05 14:22:45 +0900

    feat: error handling

--- src/pages/TodayHabitPage.jsx
+++ src/pages/TodayHabitPage.jsx
@@ -1,9 +1,12 @@
+import { memo } from 'react';
 import TodayHabitTemplate from '../template/TodayHabitTemplate';
 
-const TodayHabitPage = () => {
+const TodayHabitPage = memo(() => {
   return (
     <TodayHabitTemplate />
   );
-};
+});
+
+TodayHabitPage.displayName = 'TodayHabitPage';
 
 export default TodayHabitPage;
